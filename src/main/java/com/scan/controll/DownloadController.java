bcb15c1fc127ddc38828763c9df2ff7903138bef
isunlight001 <903635811@qq.com>
2018-12-13 11:10:45 +0800

    download change

--- src/main/java/com/scan/controll/DownloadController.java
+++ src/main/java/com/scan/controll/DownloadController.java
@@ -22,42 +22,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 2018年12月12日
  */
 @Controller
-@RequestMapping("/down") 
+@RequestMapping("/file") 
 public class DownloadController {
-	
+	/**
+	 * 使用组件下载，可以兼容的格式比较多
+	 * 设置filename和path进行传参
+	 * 2018年12月13日@return
+	 * 2018年12月13日@throws IOException
+	 */
 	@RequestMapping(value = "/downfile")
-    public ResponseEntity<byte[]> export() throws IOException {  
+    public ResponseEntity<byte[]> downloadFile() throws IOException { 
         HttpHeaders headers = new HttpHeaders();    
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);    
         headers.setContentDispositionFormData("attachment", "git命令.png");    
         return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(new File(
-        		"/Users/apple/Downloads/git命令.png")),    
+        		"/Users/apple/Downloads/git命令.png")),   
                                               headers, HttpStatus.CREATED);    
     }  
    
-    @RequestMapping(value = "/downfile1")
+    @RequestMapping(value = "/downfile1",produces = "application/octet-stream;charset=UTF-8")
     public void download(HttpServletRequest request, 
     	      HttpServletResponse response, String storeName, String contentType1
     	       ) throws Exception {  
     	    request.setCharacterEncoding("UTF-8");  
     	    BufferedInputStream bis = null;  
     	    BufferedOutputStream bos = null;  
-    	  
     	    //获取项目根目录
-    	    String ctxPath = request.getSession().getServletContext()  
-    	        .getRealPath("");  
-    	    
+//    	    String ctxPath = request.getSession().getServletContext()  
+//    	        .getRealPath("");  
     	    //获取下载文件露肩
 //    	    String downLoadPath = ctxPath+"/uploadFile/"+ storeName;  
     	    String downLoadPath = "/Users/apple/Downloads/git命令.png\"";  
     	  
     	    //获取文件的长度
     	    long fileLength = new File(downLoadPath).length();  
-
     	    //设置文件输出类型
     	    response.setContentType("application/octet-stream");  
-    	    response.setHeader("Content-disposition", "attachment; filename="  
-    	        + new String(storeName.getBytes("utf-8"), "ISO8859-1")); 
+    	    response.setHeader("Content-disposition", "attachment; filename=11.png"  
+    	        ); 
     	    //设置输出长度
     	    response.setHeader("Content-Length", String.valueOf(fileLength));  
     	    //获取输入流
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello World!</h2><br>
+<a href="${website}file/downfile1">download</a>
 </body>
 </html>
